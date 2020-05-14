792f2416c4ea7433679de1f496542b1ec3112612
SilviH <hrbkova.s@email.cz>
2020-05-14 21:34:07 +0200

    creation of all possible rotations
    creation of reset() function to reset representation

--- shape.py
+++ shape.py
@@ -1,22 +1,22 @@
-#TODO - rotatin info about 180
 
 class Shape:
     letter_name = ''
-    rotation = 0
-    is_mirror_x = False
-    is_mirror_y = False
+    init_representation = 0
     representation = 0
 
     def __init__(self, name, number):
         self.letter_name = name
-        self.representation = number
+        self.init_representation = self.representation = number
 
     def get_number(self):
         return self.representation
 
     def get_name(self):
         return self.letter_name
-
+    
+    def reset(self):
+        self.representation = self.init_representation
+        
     def to_binary(self):  # returns string
         retval = (str(bin(self.representation)))[2:]
         retval = (16 - len(retval)) * '0' + retval
@@ -35,7 +35,6 @@ class Shape:
                         "0100": "0010", "0101": "1010", "0110": "0110", "0111": "1110",
                         "1000": "0001", "1001": "1001", "1010": "0101", "1011": "1101",
                         "1100": "0011", "1101": "1011", "1110": "0111", "1111": "1111"}
-        self.is_mirror_y = not self.is_mirror_y
         binary_representation = self.to_binary()
         mirror = ''
         for row_start in range(0, 16, 4):
@@ -44,7 +43,6 @@ class Shape:
         self.representation = int(mirror, 2)
 
     def mirror_x(self):
-        self.is_mirror_x = not self.is_mirror_x
         binary_representation = self.to_binary()
         mirror = ''
         for row_start in range(12, -1, -4):
@@ -52,7 +50,6 @@ class Shape:
         self.representation = int(mirror, 2)
         
     def rotate(self, angle):
-        self.rotation = angle
         binary_representation = self.to_binary()
         rotation = list(16 * '0')
         for y in range(4):
@@ -64,12 +61,14 @@ class Shape:
         self.representation = int(''.join(rotation), 2)          
         if angle == 90:
             self.mirror_x()
-            self.is_mirror_x = not self.is_mirror_x
         if angle == 270:
-            self.mirror_y()
-            self.is_mirror_y = not self.is_mirror_y         
-            
+            self.mirror_y()   
+    
+    def get_all_orientations(self):  
+        return ['rot_0','rot_90','rot_180','rot_270','mirror_x','mirr_rot_90','mirr_rot_180','mirr_rot_270', 'mirror_y']
+        
     def set_orientation(self, orientation):
+        self.reset()
         if orientation == 'rot_0':
             return
         if orientation == 'mirror_x':
@@ -81,13 +80,20 @@ class Shape:
         if orientation == 'rot_180':
             self.mirror_x()
             self.mirror_y()
-            self.is_mirror_y = not self.is_mirror_y
-            self.is_mirror_x = not self.is_mirror_x
-            self.rotation = 180
         if orientation == 'rot_270':
+            self.rotate(270)
+        if orientation == 'mirr_rot_90':
+            self.mirror_x()
+            self.rotate(90)
+        if orientation == 'mirr_rot_180':
+            self.mirror_y()
+        if orientation == 'mirr_rot_270':
+            self.mirror_x()
             self.rotate(270)
             
 if __name__ == "__main__":
     my_shape = Shape('A', 0x44c0)
-    my_shape.set_orientation('rot_180')
-    my_shape.display()
+
+    for rotation in my_shape.get_all_orientations():
+        my_shape.set_orientation(rotation)
+        my_shape.display()
