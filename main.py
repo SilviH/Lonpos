05584be8dbae4793d8d6cd18f0a6b5402e6100cc
SilviH <hrbkova.s@email.cz>
2020-05-14 21:01:59 +0200

    rotation of shapes
    function rotate(...) created in shape.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#TO DO:
+# Stop the cycle when filled
+
 import copy
 import shape
 
@@ -10,13 +13,29 @@ initial_playfiel5 = (['H', 'H', 'B', 'B', 'B', 'C', 'C', 'C', 'C', 'K', 'K'],
                      ['D', 'J', 'J', 'J', 'J', 'L', 'I', 'I', '.', '.', '.']
                      )
 
-# riddle 005
+# riddle 006
 initial_playfield = (['J', 'J', 'J', 'J', 'H', 'H', 'D', 'D', 'D', 'D', 'F'],
                      ['C', 'C', 'I', 'I', 'L', 'H', 'H', 'D', 'E', 'F', 'F'],
                      ['C', 'G', 'I', 'L', 'L', 'L', 'H', 'E', 'E', '.', '.'],
                      ['C', 'G', 'I', 'I', 'L', 'B', 'B', 'E', '.', '.', '.'],
                      ['C', 'G', 'G', 'G', 'B', 'B', 'B', 'E', '.', '.', '.']
                      )
+
+# riddle 007 needs vertical mirror
+initial_playfield = (['E', 'E', 'G', 'G', 'G', 'D', 'D', 'D', 'D', 'K', 'K'],
+			 ['J', 'E', 'E', 'E', 'G', 'C', 'D', 'H', 'H', 'K', 'K'],
+			 ['J', 'I', 'I', 'L', 'G', 'C', 'H', 'H', '.', '.', '.'],
+			 ['J', 'I', 'L', 'L', 'L', 'C', 'H', 'F', '.', '.', '.'],
+			 ['J', 'I', 'I', 'L', 'C', 'C', 'F', 'F', '.', '.', '.']
+			 )
+
+## riddle 008
+#initial_playfield = (['B', 'B', 'D', 'D', 'D', 'D', 'H', 'H', 'A', 'A', 'A'],
+#			 ['B', 'B', 'B', 'F', 'D', 'H', 'H', 'E', 'E', 'E', 'A'],
+#			 ['I', 'I', 'L', 'F', 'F', 'H', 'E', 'E', '.', '.', '.'],
+#			 ['I', 'L', 'L', 'L', 'C', 'C', 'C', 'C', '.', '.', '.'],
+#			 ['I', 'I', 'L', 'J', 'J', 'J', 'J', 'C', '.', '.', '.']
+#			 )
 
 all_shapes = {'A': 0x44c0, 'B': 0x4cc0, 'C': 0x444c, 'D': 0x44c4,
               'E': 0x44c8, 'F': 0x4c00, 'G': 0x22e0, 'H': 0x26c0,
@@ -85,7 +104,7 @@ def insert_shape(playfield, current_shape, x, y):
 
 
 def recurse_process_shape(playfield, current_shape, remaining_shapes):
-    for orientation in [0, 'mirror_x']:
+    for orientation in ['rot_0', 'mirror_x']:
 
         current_shape.set_orientation(orientation)
         current_shape.display()
--- shape.py
+++ shape.py
@@ -1,3 +1,5 @@
+#TODO - rotatin info about 180
+
 class Shape:
     letter_name = ''
     rotation = 0
@@ -33,7 +35,7 @@ class Shape:
                         "0100": "0010", "0101": "1010", "0110": "0110", "0111": "1110",
                         "1000": "0001", "1001": "1001", "1010": "0101", "1011": "1101",
                         "1100": "0011", "1101": "1011", "1110": "0111", "1111": "1111"}
-        self.is_mirror_y = True
+        self.is_mirror_y = not self.is_mirror_y
         binary_representation = self.to_binary()
         mirror = ''
         for row_start in range(0, 16, 4):
@@ -42,21 +44,50 @@ class Shape:
         self.representation = int(mirror, 2)
 
     def mirror_x(self):
-        self.is_mirror_x = True
+        self.is_mirror_x = not self.is_mirror_x
         binary_representation = self.to_binary()
         mirror = ''
         for row_start in range(12, -1, -4):
             mirror += binary_representation[row_start: row_start + 4]
         self.representation = int(mirror, 2)
-
+        
+    def rotate(self, angle):
+        self.rotation = angle
+        binary_representation = self.to_binary()
+        rotation = list(16 * '0')
+        for y in range(4):
+            for x in range(4):
+                offset = x + 4 * y
+                if binary_representation[offset] == '1':
+                    offset2 = y + 4 * x
+                    rotation[offset2] = '1'  
+        self.representation = int(''.join(rotation), 2)          
+        if angle == 90:
+            self.mirror_x()
+            self.is_mirror_x = not self.is_mirror_x
+        if angle == 270:
+            self.mirror_y()
+            self.is_mirror_y = not self.is_mirror_y         
+            
     def set_orientation(self, orientation):
-        if orientation == 0:
+        if orientation == 'rot_0':
             return
         if orientation == 'mirror_x':
             self.mirror_x()
-
-
+        if orientation == 'mirror_y':
+            self.mirror_y()
+        if orientation == 'rot_90':
+            self.rotate(90)
+        if orientation == 'rot_180':
+            self.mirror_x()
+            self.mirror_y()
+            self.is_mirror_y = not self.is_mirror_y
+            self.is_mirror_x = not self.is_mirror_x
+            self.rotation = 180
+        if orientation == 'rot_270':
+            self.rotate(270)
+            
 if __name__ == "__main__":
     my_shape = Shape('A', 0x44c0)
-    my_shape.mirror_y()
+    my_shape.set_orientation('rot_180')
     my_shape.display()
